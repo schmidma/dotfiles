f4507f87924f24c1e0ea1534b775177053498f7f
Maximilian Schmidt <maximilian@schmidt.so>
2021-12-01 00:56:37 +0100

    Unified floating window borders, fix diagnostic signs

--- .config/nvim/lua/visuals.lua
+++ .config/nvim/lua/visuals.lua
@@ -10,11 +10,8 @@ vim.cmd [[colorscheme base16-default-dark]]
 
 vim.api.nvim_command('hi StatusLine guibg=#282828')
 
-vim.lsp.handlers["textDocument/hover"] =
-    vim.lsp.with(vim.lsp.handlers.hover, {border = "single"})
+vim.diagnostic.config({float = {border = "rounded"}})
 
-vim.lsp.handlers["textDocument/signatureHelp"] =
-    vim.lsp.with(vim.lsp.handlers.signature_help, {border = "single"})
 
 vim.g.lsp_utils_location_opts = {mode = 'editor'}
 vim.g.lsp_utils_symbols_opts = {mode = 'editor'}
@@ -27,6 +24,6 @@ local signs = {
 }
 
 for type, icon in pairs(signs) do
-  local hl = "LspDiagnosticsSign" .. type
+  local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
 end
