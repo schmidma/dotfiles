524dba27fca0b893319a53064cea9082fd7be535
Maximilian Schmidt <maximilian@schmidt.so>
2022-02-28 10:33:03 +0100

    neovim: julia filetype has 4 spaces indentation

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -4,4 +4,6 @@ augroup remember_cursor_position
     autocmd!
     autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
 augroup END
+
+"autocmd FileType julia shiftwidth=4 tabstop=4 end
 ]])
