9d9d15d6d6c998505ffd3c4065ff2506dfd4d385
Maximilian Schmidt <maximilian@schmidt.so>
2022-07-26 12:30:49 +0200

    fish: hulks rules and twix alias

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -1,4 +1,5 @@
 alias pdf zathura
 alias dotfiles '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias mailsync 'systemctl --user start offlineimap-oneshot.service'
-
+alias rules 'pdf ~/Documents/HULKs/SPL-Rules-master.pdf'
+alias twix '~/worktree/hulks/nao/twix'
