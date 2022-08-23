5218f53a5c22379bd92b9b1b31f4b09e7d75151e
Maximilian Schmidt <maximilian@schmidt.so>
2022-08-23 17:22:12 +0200

    fish: gopass as pass alias

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -3,3 +3,4 @@ alias dotfiles '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias mailsync 'systemctl --user start offlineimap-oneshot.service'
 alias rules 'pdf ~/Documents/HULKs/SPL-Rules-master.pdf'
 alias twix '~/worktree/hulks/nao/twix'
+alias pass gopass
