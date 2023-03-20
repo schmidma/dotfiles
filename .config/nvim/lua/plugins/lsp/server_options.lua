a73fea6b7c57c166ae50e55770a8b5305d92a250
Maximilian Schmidt <maximilian@schmidt.so>
2023-03-20 23:22:43 +0100

    neovim: formatting

--- .config/nvim/lua/plugins/lsp/server_options.lua
+++ .config/nvim/lua/plugins/lsp/server_options.lua
@@ -1,55 +1,55 @@
 local options = {
-  ["rust_analyzer"] = {
-    tools = {
-      inlay_hints = {
-        auto = false,
-      },
-    },
-    server = {
-      settings = {
-        ["rust-analyzer"] = {
-          cargo = {
-            features = "all",
-          },
-          assist = {
-            importEnforceGranularity = true,
-          },
-          checkOnSave = {
-            command = "clippy",
-          },
-        },
-      },
-    },
-  },
-  ["lua_ls"] = {
-    settings = {
-      Lua = {
-        runtime = {
-          version = "LuaJIT",
-        },
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          library = vim.api.nvim_get_runtime_file("", true),
-        },
-        telemetry = {
-          enable = false,
-        },
-      },
-    },
-  },
-  ["texlab"] = {
-    settings = {
-      texlab = {
-        auxDirectory = "output",
-        chktex = {
-          onEdit = true,
-          onOpenAndSave = true,
-        }
-      }
-    }
-  }
+	["rust_analyzer"] = {
+		tools = {
+			inlay_hints = {
+				auto = false,
+			},
+		},
+		server = {
+			settings = {
+				["rust-analyzer"] = {
+					cargo = {
+						features = "all",
+					},
+					assist = {
+						importEnforceGranularity = true,
+					},
+					checkOnSave = {
+						command = "clippy",
+					},
+				},
+			},
+		},
+	},
+	["lua_ls"] = {
+		settings = {
+			Lua = {
+				runtime = {
+					version = "LuaJIT",
+				},
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				telemetry = {
+					enable = false,
+				},
+			},
+		},
+	},
+	["texlab"] = {
+		settings = {
+			texlab = {
+				auxDirectory = "output",
+				chktex = {
+					onEdit = true,
+					onOpenAndSave = true,
+				},
+			},
+		},
+	},
 }
 
 return options
