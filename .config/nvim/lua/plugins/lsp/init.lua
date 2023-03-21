d13303ecf4854efd400d37a5fd536ea6f752ee78
Maximilian Schmidt <maximilian@schmidt.so>
2023-03-21 15:31:17 +0100

    neovim: run misspell only on save

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -50,8 +50,18 @@ return {
 		},
 		config = function(_, opts)
 			require("mason-null-ls").setup(opts)
-			require("null-ls").setup({})
-			require("mason-null-ls").setup_handlers({})
+			local null_ls = require("null-ls")
+			null_ls.setup({})
+			require("mason-null-ls").setup_handlers({
+				function(source_name, methods)
+					require("mason-null-ls.automatic_setup")(source_name, methods)
+				end,
+				misspell = function(_, methods)
+					null_ls.register(null_ls.builtins.diagnostics.misspell.with({
+						method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+					}))
+				end,
+			})
 		end,
 	},
 	{
