2f9dc2d8f5f5f2c26f5f60d58095da989f8e89c5
Maximilian Schmidt <maximilian@schmidt.so>
2023-05-09 17:00:25 +0200

    fish: mailsync alias to use mbsync

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -1,6 +1,6 @@
 alias pdf zathura
 alias dotfiles '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias mailsync 'systemctl --user start offlineimap-oneshot.service'
+alias mailsync 'systemctl --user start mbsync.service'
 alias rules 'pdf ~/Documents/HULKs/SPL-Rules-master.pdf'
 alias twix '~/worktree/hulks/nao/twix'
 alias pass gopass
