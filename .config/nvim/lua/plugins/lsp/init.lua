f2c1be598bcfeba8e28b14b0625340c45cec7ca4
Maximilian Schmidt <maximilian@schmidt.so>
2023-06-22 10:51:38 +0200

    neovim: venvselector + copilot

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -121,4 +121,16 @@ return {
 			},
 		},
 	},
+	{
+		"linux-cultist/venv-selector.nvim",
+		dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
+		config = true,
+		event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+		opts = {
+			name = { "venv", ".venv" },
+		},
+	},
+	{
+		"github/copilot.vim",
+	},
 }
