1e1b218cf994070af7460e1df85de83151560acd
Maximilian Schmidt <maximilian@schmidt.so>
2023-10-24 20:43:05 +0200

    fish: update aliases

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -2,6 +2,6 @@ alias pdf zathura
 alias dotfiles '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias mailsync 'systemctl --user start mbsync.service'
 alias rules 'pdf ~/Documents/HULKs/SPL-Rules-master.pdf'
-alias twix '~/worktree/hulks/nao/twix'
 alias pass gopass
 alias gpc "gopass -c"
+alias exa eza
