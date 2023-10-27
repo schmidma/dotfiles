66d538cd70ad361ad37bfbff2dcc24ec1adfe007
Maximilian Schmidt <maximilian@schmidt.so>
2023-10-27 14:24:23 +0200

    neovim: fzy_native

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -13,8 +13,7 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+				"nvim-telescope/telescope-fzy-native.nvim",
 			},
 		},
 		keys = {
@@ -77,7 +76,7 @@ return {
 		},
 		config = function(_, opts)
 			require("telescope").setup(opts)
-			require("telescope").load_extension("fzf")
+			require("telescope").load_extension("fzy_native")
 		end,
 	},
 }
