c78a1b2b59c65936b3389ee5bfb5be9c0c389603
Maximilian Schmidt <maximilian@schmidt.so>
2024-03-18 10:47:56 +0100

    neovim: automatic venv selection on Enter

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -29,6 +29,18 @@ local function setup_diagnostic_signs()
 	end
 end
 
+vim.api.nvim_create_autocmd("VimEnter", {
+	desc = "Auto select virtualenv Nvim open",
+	pattern = "*",
+	callback = function()
+		local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
+		if venv ~= "" then
+			require("venv-selector").retrieve_from_cache()
+		end
+	end,
+	once = true,
+})
+
 return {
 	{
 		"williamboman/mason.nvim",
