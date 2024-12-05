5b70a64ea622fb9356ee60d7a865ddb9756f6ce2
Maximilian Schmidt <maximilian@schmidt.so>
2024-12-05 14:59:37 +0100

    nvim: migrate to kanagawa colors

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,72 +1,6 @@
 local keys = require("keys")
 
-local function get_color(group, attr)
-	return vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(group)), attr)
-end
-
-local function theme_postfix()
-	local colors = require("base16-colorscheme").colors
-	local highlight = require("base16-colorscheme").highlight
-
-	local bg_gutter = colors.base01
-
-	highlight.LineNr = {
-		guibg = bg_gutter,
-		guifg = get_color("LineNr", "fg"),
-	}
-	highlight.SignColumn = {
-		guibg = bg_gutter,
-		guifg = get_color("SignColumn", "fg"),
-	}
-	highlight.DiagnosticSignError = {
-		guibg = bg_gutter,
-		guifg = get_color("DiagnosticError", "fg"),
-	}
-	highlight.DiagnosticSignWarn = {
-		guibg = bg_gutter,
-		guifg = get_color("DiagnosticWarn", "fg"),
-	}
-	highlight.DiagnosticSignHint = {
-		guibg = bg_gutter,
-		guifg = get_color("DiagnosticHint", "fg"),
-	}
-	highlight.DiagnosticSignInfo = {
-		guibg = bg_gutter,
-		guifg = get_color("DiagnosticInfo", "fg"),
-	}
-	highlight.GitGutterAdd = {
-		guibg = bg_gutter,
-		guifg = get_color("GitGutterAdd", "fg"),
-	}
-	highlight.GitGutterChange = {
-		guibg = bg_gutter,
-		guifg = get_color("GitGutterChange", "fg"),
-	}
-	highlight.GitGutterDelete = {
-		guibg = bg_gutter,
-		guifg = get_color("GitGutterDelete", "fg"),
-	}
-	highlight.GitGutterChangeDelete = {
-		guibg = bg_gutter,
-		guifg = get_color("GitGutterChangeDelete", "fg"),
-	}
-end
-
 return {
-	{
-		"RRethy/nvim-base16",
-		opts = {
-			theme = "base16-default-dark",
-		},
-		config = function(_, opts)
-			vim.api.nvim_create_autocmd("Colorscheme", {
-				group = vim.api.nvim_create_augroup("colorscheme-postfixes", {}),
-				callback = theme_postfix,
-			})
-
-			vim.cmd.colorscheme(opts.theme)
-		end,
-	},
 	{
 		"RRethy/vim-illuminate",
 		event = { "BufReadPost", "BufNewFile" },
@@ -89,5 +23,24 @@ return {
 				desc = "Previous Reference",
 			},
 		},
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		config = function()
+			local kanagawa = require("kanagawa")
+			kanagawa.setup({
+				dimInactive = true,
+				overrides = function(colors)
+					local theme = colors.theme
+					return {
+						Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+						PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+						PmenuSbar = { bg = theme.ui.bg_m1 },
+						PmenuThumb = { bg = theme.ui.bg_p2 },
+					}
+				end,
+			})
+			kanagawa.load()
+		end,
 	},
 }
