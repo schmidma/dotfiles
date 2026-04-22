8a1640aba2b024b9fbf9d8f2667be27d8283f01c
Maximilian Schmidt <maximilian@schmidt.so>
2026-04-22 10:53:55 +0200

    fish: no flatpak aliases

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -5,12 +5,8 @@ alias rules 'pdf ~/Documents/HULKs/SPL-Rules-master.pdf'
 alias pass gopass
 alias gpc "gopass -c"
 alias exa eza
-alias evince "flatpak run org.gnome.Evince"
-alias okular "flatpak run org.kde.okular"
 alias tree "exa --tree --level=3"
 alias o "gio open"
 alias logs_download "wget -m -np -nH -c -R \"index.html\""
-alias mpv "flatpak run io.mpv.Mpv"
-alias firefox "flatpak run org.mozilla.firefox"
-alias zen "flatpak run app.zen_browser.zen"
+alias zen "zen-browser"
 alias tcm "cd ~/worktree/hulks/GameController/RoboCup2025/GameController/ && _JAVA_AWT_WM_NONREPARENTING=1 java -jar TeamCommunicationMonitor.jar"
